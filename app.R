56bbbf89be0746b40343d4502551a1a7ca1eb1ac
kevingunalan <85707940+kevingunalan@users.noreply.github.com>
2022-07-03 07:35:42 -0400

    Updated the R script to include tabs
    Tabs - Simulation Setting & File Upload
    Attached sample time series CSV file

--- app.R
+++ app.R
@@ -5,7 +5,12 @@ library(viridis)
 library(signal)
 library(fossil)
 
+wd = "C:/Users/Administrator/Desktop/Summer Project/"
+setwd(wd)
+
 source("EBA_functions.R")
+df <- read.csv("xtsample.csv")
+df <- df[[1]]
 
 ui <- fluidPage(
   tags$h1("Empirical Frequency Band Analysis"),
@@ -18,6 +23,8 @@ ui <- fluidPage(
   tags$hr(),
   sidebarLayout(
     sidebarPanel(
+      conditionalPanel(condition = "input.tabselected==1",
+      
       selectInput("Simsetting", "Simulation Setting",
                   c("White Noise" = "W",
                     "Linear" = "L",
@@ -26,7 +33,7 @@ ui <- fluidPage(
                   value = 50000, min = 1000, max = 50000, step = 1000),
       
       sliderInput(inputId = "Num", label = "Choose number of observations per approximately stationary block", 
-                  value = 500, min = 100, max = 1000, step = 100),
+                  value = 500, min = 50, max = 1000, step = 100),
       
       sliderInput(inputId = "Tapers", label = "Choose number of tapers to use in multitaper spectral estimator", 
                   value = 15, min = 0, max = 50, step = 5),
@@ -36,18 +43,74 @@ ui <- fluidPage(
       
       radioButtons(inputId = "TF", label = "Standardize", 
                    c("True" = TRUE, "False" = FALSE), selected = FALSE),
-      actionButton("go", label = "Run")
+      actionButton("go", label = "Run")),
+      
+      conditionalPanel(condition = "input.tabselected==2",
+      fileInput("file_csv", "Choose CSV File",
+                multiple = TRUE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+      checkboxInput("header", "Header", TRUE),
+      sliderInput(inputId = "Num2", label = "Choose number of observations per approximately stationary block", 
+                  value = max(floor(length(df)/6),50), min = 50, max = length(df), step = 1),
+      
+      sliderInput(inputId = "Tapers2", label = "Choose number of tapers to use in multitaper spectral estimator", 
+                  value = 15, min = 0, max = floor(2*length(df)*.15-1), step = 1),
+      
+      radioButtons(inputId = "Signi2", label = "Choose significance level", 
+                   c("0.05" = 0.05, "0.01" = 0.01), selected = "0.05"),
+      
+      radioButtons(inputId = "TF2", label = "Standardize", 
+                   c("True" = TRUE, "False" = FALSE), selected = FALSE),
+      actionButton("go2", label = "Run"))
     ),
     mainPanel(
-      plotOutput("Image_Plot")
-    )
+      tabsetPanel(type = "tabs", id = 'tabselected', selected = 1,
+                  tabPanel("Simulation Setting", value = 1),
+                  tabPanel("File Upload", value = 2)),
+      conditionalPanel(condition = "input.tabselected==1",
+      plotOutput("Image_Plot"),
+      radioButtons(inputId = "downloadType", label = "Select download type", choices = list("png","pdf")),
+      downloadButton('downloadData','Download the plot')
+      ),
+      conditionalPanel(condition = "input.tabselected==2",
+                       plotOutput("Image_Plot2"),
+                       ))
   ))
 
 server <- function(input,output) {
-  plot.list <- eventReactive(input$go, ignoreNULL = FALSE, {
-    X = eba.simdata(T=input$Time)
+  plot.list2 <- eventReactive(input$go2, ignoreNULL = FALSE, {
+    file <- input$file_csv
+    ext <- tools::file_ext(file$datapath)
+    req(file)
+    validate(need(ext == "csv", "Please upload a csv file"))
+    dataf <- read.csv(file$datapath, header = input$header)
+    dataf <- dataf[[1]]
+    ebaoutfu <- eba.search(X=dataf,N= as.numeric(input$Num2),K=as.numeric(input$Tapers2),std=input$TF2,alpha=as.numeric(input$Signi2))
+    plot.x = ebaoutfu$mtspec$t
+    plot.y = ebaoutfu$mtspec$f
+    plot.z = t(ebaoutfu$mtspec$mtspec)
+    plot.main = "Multitaper Spectrogram"
+    plot.h = ebaoutfu$part.final[c(-1,-length(ebaoutfu$part.final))]
+    plot.data = dataf
+    list(plot.x = plot.x, plot.y = plot.y, plot.z = plot.z, 
+         plot.main = plot.main, plot.h = plot.h, plot.data = plot.data)})
     
-  
+    
+    output$Image_Plot2 <- renderPlot({
+      par(mfrow=c(2,1))
+      plot.ts(plot.list2()[[6]])
+      image.plot(x=plot.list2()[[1]], y=plot.list2()[[2]], z=plot.list2()[[3]], 
+                 axes = TRUE, col = inferno(256), 
+                 xlab='Time',ylab='Hz',xaxs="i", main = plot.list2()[[4]]); 
+      abline(h=plot.list2()[[5]], col = "green");
+      })
+    
+    
+  plot.list <- eventReactive(input$go, ignoreNULL = FALSE, {
+      X = eba.simdata(T=input$Time)
+      
   if (input$Simsetting == "W"){
     ebaout.wn <- eba.search(X=X$wn,N= as.numeric(input$Num),K=as.numeric(input$Tapers),std=input$TF,alpha=as.numeric(input$Signi))
     plot.x = ebaout.wn$mtspec$t
@@ -93,6 +156,26 @@ server <- function(input,output) {
                axes = TRUE, col = inferno(256), 
                xlab='Time',ylab='Hz',xaxs="i", main = plot.list()[[4]]); 
     abline(h=plot.list()[[5]], col = "green");
+    
+    output$downloadData <- downloadHandler(
+      filename = function(){
+        paste("White_noise",input$downloadType,sep = ".") 
+      },
+      content = function(file){
+        if(input$downloadType == "png")
+          png(file)
+        else
+          pdf(file)
+        par(mfrow=c(2,1))
+        plot(x = seq(0,1,length.out = length(plot.list()[[6]])), y = plot.list()[[6]], type = "l", xlab = "Time", ylab = "")
+        image.plot(x=plot.list()[[1]], y=plot.list()[[2]], z=plot.list()[[3]], 
+                   axes = TRUE, col = inferno(256), 
+                   xlab='Time',ylab='Hz',xaxs="i", main = plot.list()[[4]]); 
+        abline(h=plot.list()[[5]], col = "green");
+        
+        dev.off()
+      }
+    )
     
     
         
